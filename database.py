30fbc06ad845f3322970997e61f1b1a8ece51f5f
daniel martinez garcia <daniel_mg_nn@hotmail.com>
2022-09-22 14:53:54 -0500

    connect to prostgresql

--- database.py
+++ database.py
@@ -1,14 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL") #"sqlite:///./todos.db"
 
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://","postgresql://",1)
 # MYSQL Series
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL #, connect_args={"check_same_thread": False}
 )
 
 # MYSQL Series
